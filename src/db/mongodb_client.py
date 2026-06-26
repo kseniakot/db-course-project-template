1a1951913689ff97d71c845d1430cbb3805abf25
kotks <kseniakotova116@gmail.com>
2026-06-26 18:57:16 +0300

    feature: create mongo indexes

--- src/db/mongodb_client.py
+++ src/db/mongodb_client.py
@@ -1,6 +1,6 @@
 """MongoDB connection and utilities."""
 
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING, DESCENDING
 from pymongo.database import Database
 
 from src.config import MONGO_CONFIG
@@ -17,8 +17,27 @@ class MongoDBClient:
 
     def create_indexes(self):
         """Create necessary indexes."""
-        # TODO: Add indexes as needed
 
+        reviews = self.get_collection("reviews")
+        reviews.create_index([("product_id", ASCENDING)])
+        reviews.create_index([("user_id", ASCENDING)])
+        reviews.create_index([("helpful_votes", DESCENDING)])
+        reviews.create_index([("product_id", ASCENDING), ("rating", DESCENDING)])
+
+        product_specs = self.get_collection("product_specs")
+        product_specs.create_index([("product_id", ASCENDING)], unique=True)
+        product_specs.create_index([("category", ASCENDING)])
+        product_specs.create_index([("specs.material", ASCENDING)])
+
+        seller_profiles = self.get_collection("seller_profiles")
+        seller_profiles.create_index([("seller_id", ASCENDING)], unique=True)
+        seller_profiles.create_index([("rating", ASCENDING)])
+
+        user_preferences = self.get_collection("user_preferences")
+        user_preferences.create_index([("user_id", ASCENDING)], unique=True)
+
+
+     
 
 # Singleton instance
 mongo_client = MongoDBClient()
